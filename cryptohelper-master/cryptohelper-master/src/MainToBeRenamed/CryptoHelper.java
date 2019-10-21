7673310f2233cd45acadaf6a2173bc6d95ae865e
jfrommeyer5 <jfrommeyer5@users.noreply.github.com>
2019-10-20 20:15:18 -0400

    [JTF] Base64Encoding
    Recreated the Base64 Encoding because why not. I then test it against Java's implementation to make sure it is functioning properly.

--- cryptohelper-master/cryptohelper-master/src/MainToBeRenamed/CryptoHelper.java
+++ cryptohelper-master/cryptohelper-master/src/MainToBeRenamed/CryptoHelper.java
@@ -15,9 +15,6 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import Menu.OptionsMenu;
-import java.util.*;
-import java.util.jar.*;
-import java.util.zip.*;
 
 /**
  *
@@ -59,6 +56,7 @@ public class CryptoHelper extends javax.swing.JFrame {
         columnTranspositionWorksheet = new ColumnTranspositionWorksheet();
         gcdAndInverse = new GCDAndInverse();
         substitutionSolver = new SubstitutionSolver();
+        base64Encode = new Base64Encode();
 
         optionsMenu = new OptionsMenu();
 
@@ -687,6 +685,29 @@ public class CryptoHelper extends javax.swing.JFrame {
         rightHandNavPanelTab.addTab("GCD and Inverse", gcdAndInverse.getMainCipherPanel());
 //end GCD
 
+//begin Jacob's Cipher thing.
+
+        base64Encode.getMainCipherTextArea().addKeyListener(new KeyAdapter() {
+            public void keyReleased(KeyEvent evt) {
+                jTextAreaKeyReleased(evt);
+            }
+        });
+        base64Encode.getMainCipherTextArea().addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent evt) {
+                jTextAreaMouseClicked(evt);
+            }
+        });
+
+        base64Encode.getActionButton().addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                base64Encode.setInputText(inputText);
+            }
+        });
+
+        rightHandNavPanelTab.addTab("Base64Encode", base64Encode.getMainCipherPanel());
+
+//end Jacob's Cipher thing.
         substitutionSolver.getMainCipherTextArea().addKeyListener(new KeyAdapter() {
             public void keyReleased(KeyEvent evt) {
                 jTextAreaKeyReleased(evt);
@@ -864,6 +885,7 @@ public class CryptoHelper extends javax.swing.JFrame {
     private ColumnTranspositionWorksheet columnTranspositionWorksheet;
     private GCDAndInverse gcdAndInverse;
     private SubstitutionSolver substitutionSolver;
+    private Base64Encode base64Encode;
 
     private OptionsMenu optionsMenu;
 
